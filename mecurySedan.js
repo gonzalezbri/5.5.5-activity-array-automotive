a33eda9d589f184200d87515cb29f4b94d5bdc32
gonzalezbri <4717bg@gmail.com>
2023-03-08 20:27:40 -0800

    fisrt commit

--- mecurySedan.js
+++ mecurySedan.js
@@ -1,6 +1,45 @@
 //this includes the vehicle class as a module
-const VehicleModule = require("./vehicleBaseClass")
+const VehicleModule = require('./vehicle.js').Vehicle
 
 //this shows how to call from this module...
-let v = new VehicleModule.Vehicle("Mecury", "Sedan", "1965", "color", "mileage");
-console.log(v.make)
+
+class Car extends VehicleModule{
+    constructor(make, model, year, color, mileage){
+        super(make, model, year, color, mileage)
+        this.maximumPassengers = 5;
+        this.passenger = 0;
+        this.numberOfWheels = 4;
+        this.maximumSpeed = 160;
+        this.fuel = 10;
+        this.scheduleService = false;
+
+    }
+    loadPassenger(Number){
+        return Number
+    }
+    start() {
+        if (this.fuel > 0) {
+            return this.started = true;
+            console.log("engine started...!!!");
+        } else {
+            return this.started = false;
+            console.log("engine cannot start...");
+        }
+    }
+    scheduledService(){
+        if (this.mileage>30000) {
+            this.scheduleService = true
+            return this.scheduleService;
+
+    }}}
+let A28MercurySedan = new Car("Mercury","A28","1965","Blue",35000)
+A28MercurySedan.start()
+A28MercurySedan.loadPassenger()
+A28MercurySedan.stop()
+A28MercurySedan.scheduledService()
+console.log(A28MercurySedan)
+
+
+
+
+
